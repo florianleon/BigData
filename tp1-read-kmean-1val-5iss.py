58ec37b113f57149f2fbb17299eea044ba58af26
Florian Leon <leon-florian@hotmail.fr>
2021-12-02 22:46:06 +0100

    Correction bug detection inertie

--- tp1-read-kmean-1val-5iss.py
+++ tp1-read-kmean-1val-5iss.py
@@ -11,6 +11,7 @@ import time
 from scipy.io import arff
 from sklearn import cluster
 from sklearn import metrics
+from sklearn import preprocessing
 
 ##################################################################
 # READ a data set (arff format)
@@ -71,14 +72,11 @@ def plot_metrics(Y, title):
 def compute_distance(X, Y):
     return np.sqrt(np.sum(np.square(X[0]-X[1]) + np.square(Y[0]-Y[1])))
 
-def find_best_index(liste, cmp, inert=False):
+def find_best_index(liste, cmp):
     distances = []
     best_index = 0
     for i in range(len(liste)):
-        if (inert == True):
-            distance = compute_distance([0.0, 0.0], [i, liste[i]])
-        else:
-            distance = compute_distance([0.0, 1.0], [i, liste[i]])
+        distance = compute_distance([0.0, cmp], [i, liste[i]])
         distances.append(distance)
 
     best_index = find_min(distances)
@@ -102,12 +100,14 @@ def get_best_k():
 
     best_silh = find_max(silhs)
     best_v_meas = find_max(v_meas)
-    best_inert = find_best_index(inerts, 0.0, True)
+    inerts = preprocessing.scale(inerts)
+    best_inert = find_best_index(inerts, 0.0)
     best_homs = find_best_index(homs, 1.0)
     # Calcul de la moyenne des metrics
-    mean_k = (best_silh + best_v_meas + best_homs) / 3
+    mean_k = (best_inert + best_silh + best_v_meas + best_homs) / 4
     # On rattrape le décalage d'index 
     best_k = round(mean_k) + 2
+    plot_metrics(inerts, "Inertie")
     """
     plot_metrics(inerts, "Inertie")
     plot_metrics(silhs, "Silhouette")
@@ -116,58 +116,27 @@ def get_best_k():
     """  
 
     print("best k is = ", best_k)
-    print("Best inert", best_inert)
-    print("Best silh", best_silh)
-    print("Best v_meas", best_v_meas)
-    print("Best homs", best_homs)
+    print("Best inert", best_inert + 2)
+    print("Best silh", best_silh + 2)
+    print("Best v_meas", best_v_meas + 2)
+    print("Best homs", best_homs + 2)
     print("------------------------------------------------------")
 
     return best_k, model_kms[best_k-2]
 
+def run_k_means():
+    tps1 = time.time()
+    k, model_km = get_best_k()
+    tps2 = time.time()
+    # Nb iteration of this method
+    iteration = model_km.n_iter_
+    labels_km = model_km.labels_
+    plt.scatter(f0, f1, c=labels_km, s=8)
+    plt.title("Données (init) après clustering")
+    plt.show()
+    print("nb clusters =",k,", nb iter =",iteration, ", runtime = ", round((tps2 - tps1)*1000,2),"ms")
 
-
-tps1 = time.time()
-
-k, model_km = get_best_k()
-tps2 = time.time()
-# Nb iteration of this method
-iteration = model_km.n_iter_
-labels_km = model_km.labels_
-#print("nb clusters =",k,", nb iter =",iteration, ", runtime = ", round((tps2 - tps1)*1000,2),"ms")
-plt.scatter(f0, f1, c=labels_km, s=8)
-plt.title("Données (init) après clustering")
-plt.show()
-print("nb clusters =",k,", nb iter =",iteration, ", runtime = ", round((tps2 - tps1)*1000,2),"ms")
-
-    
-
-"""
-#k=3
-model_km = cluster.KMeans(n_clusters=k, init='k-means++')
-model_km.fit(datanp)
-tps2 = time.time()
-labels_km = model_km.labels_
-# Nb iteration of this method
-iteration = model_km.n_iter_
-
-# Résultat du clustering
-plt.scatter(f0, f1, c=labels_km, s=8)
-plt.title("Données (init) après clustering")
-plt.show()
-print("nb clusters =",k,", nb iter =",iteration, ", runtime = ", round((tps2 - tps1)*1000,2),"ms")
-#print("labels", labels_km)
-# Some evaluation metrics
-# inertie = wcss : within cluster sum of squares
-inert = model_km.inertia_
-silh = metrics.silhouette_score(datanp, model_km.labels_, metric='euclidean')
-hom = metrics.homogeneity_score(ground_truth, model_km.labels_)
-v_mea = metrics.v_measure_score(ground_truth, model_km.labels_)
-print("Inertie : ", inert)
-print("Coefficient de silhouette : ", silh)
-print("Coefficient d'homogénéité : ", hom)
-print("Coefficient de v-mesure : ", v_mea)
-"""
-
+run_k_means()
 ########################################################################
 # TESTER PARAMETRES METHODE ET RECUPERER autres métriques
 ########################################################################
